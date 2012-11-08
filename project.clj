af4af82fb38e1000eaf74022c80846259238514e
Micha Niskin <asdf@ubergibson.com>
2012-11-07 19:26:47 -0500

    init

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hlisp-jayq "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/micha/hlisp-jayq"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :manifest {"hlisp-provides" "jayq"}
