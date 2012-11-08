bb100cf174c973bebd2ee31008c64a4ce03a0afc
Micha Niskin <asdf@ubergibson.com>
2012-11-07 19:25:35 -0500

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hlisp-jayq "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :manifest {"hlisp-provides" "jayq"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hlisp-jquery "0.1.0-SNAPSHOT"]])
---
+++ test/hlisp_jayq/core_test.clj
@@ -0,0 +1,7 @@
+(ns hlisp-jayq.core-test
+  (:use clojure.test
+        hlisp-jayq.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
